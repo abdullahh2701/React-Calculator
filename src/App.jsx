84581b9989a1c850010b6632463340d91a21cfb4
Abdullah Islam <abdullahislam2701@gmail.com>
2025-02-27 21:16:03 +0500

    made the app fully functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import styles from "./App.module.css";
 import ButtonsContainer from "./components/ButtonsContainer";
 import Display from "./components/Display";
-
+import { useState } from "react";
 function App() {
+  const [calVal, setCalVal] = useState("");
+  const onButtonClick = (event) => {
+    let buttonText = event.target.innerText;
+
+    if (buttonText === "C") {
+      setCalVal("");
+    } else if (buttonText === "=") {
+      const result = eval(calVal);
+      setCalVal(result);
+    } else {
+      const newDisplayValue = calVal + buttonText;
+      setCalVal(newDisplayValue);
+    }
+  };
   return (
     <div className={styles.calculator}>
-      <Display />
-      <ButtonsContainer />
+      <Display displayValue={calVal} />
+      <ButtonsContainer onButtonClick={onButtonClick} />
     </div>
   );
 }
